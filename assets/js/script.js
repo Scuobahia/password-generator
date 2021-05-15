684a282b1c38cc57a4dff0465a92fae67e2427c8
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-05-15 11:00:20 -0600

    more java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+//DOM
+const resultEl=document.getElementById ('result');
+const lengthEl=document.getElementById ('length');
+const uppercaseEl=document.getElementById ('uppercase');
+const lowercaseEl=document.getElementById ('lowercase');
+const numbersEl=document.getElementById ('numbers');
+const symbolsEl=document.getElementById ('symbols');
+const generateEl=document.getElementById ('generate');
+const clipboardEl=document.getElementById ('clipboard');
+
+const randomFunction = {
+    lower:getRandomLower,
+    upper:getRandomUpper,
+    number:getRandomNumber,
+    symbol:getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+	const length = lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+});
+
+
 // Lower Case 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -14,4 +41,4 @@ function getRandomNumber () {
 function getRandomSymbol() {
     return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 }
-console.log (getRandomSymbol());
+
