7fac2610519caef8b233be3a0914ec6e3a7abacd
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-05-15 16:58:47 -0600

    Variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 
+// Var
+var lengthEl = document.getElementById ('length').Value;
+var uppercaseEl = document.getElementById('uppercase').checked;
+var lowercaseEl = document.getElementById('lowercase').checked;
+var numbersEl = document.getElementById('numbers').checked;
+var symbolsEl = document.getElementById('symbols').checked;
