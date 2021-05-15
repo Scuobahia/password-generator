1b39e66a5748898c0cd218f58cf520598a228cb1
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-05-15 17:04:22 -0600

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,13 @@ var uppercaseEl = document.getElementById('uppercase').checked;
 var lowercaseEl = document.getElementById('lowercase').checked;
 var numbersEl = document.getElementById('numbers').checked;
 var symbolsEl = document.getElementById('symbols').checked;
+
+//Generate Password
+function generateCode() {
+lengthEl = document.getElementById('length').value;
+uppercaseEl = document.getElementById('uppercase').checked;
+lowercaseEl = document.getElementById('lowercase').checked;
+numbersEl = document.getElementById('numbers').checked
+symbolsEl = document.getElementById('symbols').checked;
+
+}
